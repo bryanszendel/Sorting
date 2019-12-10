f0e04ca10ec416d3021f8192f6a89a7b67437e91
Bryan Szendel <bryan.szendel@gmail.com>
2019-12-10 15:51:41 -0700

    removed incrementor in bubble_sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -23,16 +23,13 @@ def selection_sort( arr ):
 def bubble_sort( arr ):
     decrementer = len(arr) 
     while decrementer != 0:
-        incrementor = 0
         for i in range(0, len(arr) -1):
             if arr[i] > arr[i + 1]:
                 temp = arr[i]
                 arr[i] = arr[i + 1]
                 arr[i + 1] = temp
-                incrementor += 1
         decrementer -= 1
     return arr
-
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
